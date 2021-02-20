028aea2bd88343a98cce0ed9a8ac997b3eb964a5
qingchuwudi <bypf2009@vip.qq.com>
2021-02-20 14:55:51 +0800

    add:支持多个公网IP（宽带多拨等情景） add:增加缓存功能 change:调整日志输出

--- config/config.go
+++ config/config.go
@@ -17,28 +17,33 @@ package config
 
 import (
 	"errors"
+	"fmt"
+	alidns "github.com/alibabacloud-go/alidns-20150109/client"
 	"io/ioutil"
 	"net/http"
 	"strings"
-	//"github.com/alibabacloud-go/tea/tea"
 	"gopkg.in/yaml.v2"
 )
 
 const (
-    IPv4Type = "A"
-    IPv6Type = "AAAA"
-    AliyunDNS = "dns.aliyuncs.com" // 修改DNS的API地址
+	IPv4Type  = "A"
+	IPv6Type  = "AAAA"
+	AliyunDNS = "dns.aliyuncs.com" // 修改DNS的API地址
 )
 
 // 配置文件
 type Config struct {
-	AccessKeyId     string     `yaml:"accessKeyId"`
-	AccessKeySecret string     `yaml:"accessKeySecret"`
-	TTL             int64      `yaml:"ttl"`
-	IPv4            string     `yaml:"ipv4_check_url"`
-	IPv6            string     `yaml:"ipv6_check_url"`
-	Interval        int        `yaml:"interval"`
+	AccessKeyId     string `yaml:"accessKeyId"`
+	AccessKeySecret string `yaml:"accessKeySecret"`
+	TTL             int64  `yaml:"ttl"`
+	IPv4            string `yaml:"ipv4_check_url"`
+	IPv6            string `yaml:"ipv6_check_url"`
+	Interval        int    `yaml:"interval"`
+	BroadbandRetry  int8   `yaml:"broadband_retry"`
+	UseCache        bool   `yaml:"cache"`
 	Customer        []Customer `yaml:"customer"`
+	IPsCache        map[string]string
+	RecordIds       map[string]string // 记录DNS的recordID
 }
 
 type Customer struct {
@@ -61,24 +66,111 @@ func (c *Config) InitConfig(file string) error {
 	if (c.IPv4 == "") && (c.IPv6 == "") {
 		return errors.New("It has at least one value of 'ipv4_check_url' and 'ipv6_check_url' in config.yaml.")
 	}
+
+	// 缓存处理
+	if c.UseCache {
+		c.IPsCache = make(map[string]string)
+		c.RecordIds = make(map[string]string)
+	} else {
+		c.IPsCache = nil
+		c.RecordIds = nil
+	}
+
 	return nil
 }
 
-func (c *Config) PublilcIPs() (PubIPv4, PubIPv6 *string) {
-	PubIPv4, PubIPv6 = nil, nil
-	if c.IPv4 != "" {
-		PubIPv4 = GetPublishIP(c.IPv4)
+// 初始化
+func (c *Config) InitBroadbandRecords() map[string]bool {
+	if c.BroadbandRetry > 0 {
+		return make(map[string]bool, c.BroadbandRetry)
 	}
-	if c.IPv6 != "" {
-		PubIPv6 = GetPublishIP(c.IPv6)
+	return nil
+}
+
+// 初始化IP缓存
+func (c *Config) InitCache(cli *alidns.Client) {
+	if !c.UseCache {
+		fmt.Println("Cache is OFF and will not be used.")
+		return
+	}
+	for _, customer := range c.Customer {
+		if (customer.IPv4RR != "") || (c.IPv4 != "") {
+			subDomain := customer.IPv4RR + "." + customer.Domain
+			c.subDomainRecordsToCache(cli, &subDomain)
+		}
+		if (customer.IPv6RR != "") || (c.IPv6 != "") {
+			subDomain := customer.IPv6RR + "." + customer.Domain
+			c.subDomainRecordsToCache(cli, &subDomain)
+		}
+	}
+}
+
+// 查询DNS记录并更新到缓存
+func (c *Config) subDomainRecordsToCache(cli *alidns.Client, subDomain *string) {
+	subDomainRequest := &alidns.DescribeSubDomainRecordsRequest{
+		SubDomain: subDomain,
+	}
+	subDomainRecords, err := cli.DescribeSubDomainRecords(subDomainRequest)
+	if err == nil {
+		// 获取域名解析记录，更新缓存
+		for _, record := range subDomainRecords.Body.DomainRecords.Record {
+			// www.yourdomain.com#A => 127.0.0.1
+			// www.yourdomain.com#AAAA => ::1
+			cacheKey := CacheKey(subDomain,record.Type)
+			c.IPsCache[cacheKey] = *record.Value
+			c.RecordIds[cacheKey] = *record.RecordId
+		}
+	}
+}
+
+func PublilcIPs(IPv4CheckUrl, IPv6CheckUrl string) (PubIPv4, PubIPv6 *string) {
+	PubIPv4, PubIPv6 = nil, nil
+	if IPv4CheckUrl != "" {
+		PubIPv4 = GetPublishIP(IPv4CheckUrl)
+	}
+	if IPv6CheckUrl != "" {
+		PubIPv6 = GetPublishIP(IPv6CheckUrl)
 	}
 	return PubIPv4, PubIPv6
 }
 
+func MultiBroadbandPublicIPs(IPv4CheckUrl, IPv6CheckUrl string, broadbandRetry int8) (map[string]bool, map[string]bool) {
+	if broadbandRetry < 2 {
+		return nil, nil
+	}
+
+	broadbandIPv4 := make(map[string]bool, 0)
+	broadbandIPv6 := make(map[string]bool, 0)
+
+	for i := int8(0); i < broadbandRetry; i++ {
+		pubIPv4, pubIPv6 := PublilcIPs(IPv4CheckUrl, IPv6CheckUrl)
+		if pubIPv4 != nil {
+			broadbandIPv4[*pubIPv4] = true
+		}
+		if pubIPv6 != nil {
+			broadbandIPv6[*pubIPv6] = true
+		}
+	}
+	if len(broadbandIPv4) == 0 {
+		broadbandIPv4 = nil
+	}
+	if len(broadbandIPv6) == 0 {
+		broadbandIPv6 = nil
+	}
+	return broadbandIPv4, broadbandIPv6
+}
+
+func BroadbandIPFisrt(broadbandIPs map[string]bool) string {
+	for broadbandIP := range broadbandIPs {
+		return broadbandIP
+	}
+	return ""
+}
 
 func GetPublishIP(IPCheckUrl string) *string {
 	resp, err := http.Get("http://" + IPCheckUrl)
 	if err != nil {
+		fmt.Printf("公网IP查询失败 ：%s\r\n", err.Error())
 		return nil
 	}
 	defer resp.Body.Close()
@@ -88,3 +180,27 @@ func GetPublishIP(IPCheckUrl string) *string {
 	return &ip2
 }
 
+func CacheKey(subDomain, ipType *string) string {
+	return (*subDomain + "#" + *ipType)
+}
+
+// 判断ip是否发生变动
+func DoesIPChanged(config *Config, broadbandIP map[string]bool, cacheKey, oldIP, newIP string) bool {
+	if config.BroadbandRetry > 1 {
+		if config.UseCache {
+			// 缓存中的IP出现在本次公网查询结果，保持解析记录不变
+			return broadbandIP[config.IPsCache[cacheKey]]
+		} else {
+			// 正在使用的IP命中查询结果集
+			return broadbandIP[oldIP]
+		}
+	} else {
+		if config.UseCache {
+			// IP在缓存中，IP 没有发生变化，不做任何操作
+			return config.IPsCache[cacheKey] == newIP
+		} else {
+			// IP 没有发生变化，不做任何操作
+			return oldIP == newIP
+		}
+	}
+}
--- main.go
+++ main.go
@@ -17,152 +17,229 @@ limitations under the License.
 package main
 
 import (
-    myConfig "aliyun-dns/config"
-    "crypto/tls"
-    "flag"
-    "fmt"
-    alidns "github.com/alibabacloud-go/alidns-20150109/client"
-    openapi "github.com/alibabacloud-go/darabonba-openapi/client"
-    "github.com/alibabacloud-go/tea/tea"
-    "net/http"
-    "os"
-    "time"
+	myConfig "aliyun-dns/config"
+	"flag"
+	"fmt"
+	alidns "github.com/alibabacloud-go/alidns-20150109/client"
+	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
+	"github.com/alibabacloud-go/tea/tea"
+	"os"
+	"time"
 )
 
-
-var tr = &http.Transport{
-    TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-}
-
-// 实际中应该用更好的变量名
 var (
-    h bool
-    c string
+	h bool
+	c string
 )
 
 func init() {
-    flag.BoolVar(&h, "h", false, "This help")
-    flag.StringVar(&c, "c", "", "Full path of the config file.")
-    flag.Usage = usage
+	flag.BoolVar(&h, "h", false, "This help")
+	flag.StringVar(&c, "c", "", "Full path of the config file.")
+	flag.Usage = usage
 }
 
 func usage() {
-    fmt.Fprintf(os.Stderr, `DDNS with Aliyun dns written in go.
+	fmt.Fprintf(os.Stderr, `DDNS with Aliyun dns written in go.
 Usage: aliyun-ddns [-h] [-c '/full/path/of/config.yaml']
 
 Options:
 `)
-    flag.PrintDefaults()
+	flag.PrintDefaults()
 }
+
+
 
 // 创建客户端
-func CreateClient(accessKeyId *string, accessKeySecret *string) (_result *alidns.Client, _err error) {
-    config := &openapi.Config{}
-    config.AccessKeyId = accessKeyId
-    config.AccessKeySecret = accessKeySecret
-    config.Endpoint = tea.String(myConfig.AliyunDNS)
-    _result = &alidns.Client{}
-    _result, _err = alidns.NewClient(config)
-    return _result, _err
+func CreateClient(accessKeyId, accessKeySecret *string) (_result *alidns.Client, _err error) {
+	config := &openapi.Config{}
+	config.AccessKeyId = accessKeyId
+	config.AccessKeySecret = accessKeySecret
+	config.Endpoint = tea.String(myConfig.AliyunDNS)
+	_result = &alidns.Client{}
+	_result, _err = alidns.NewClient(config)
+	return _result, _err
 }
 
-func run(access myConfig.Config) {
-    cli, err := CreateClient(tea.String(access.AccessKeyId), tea.String(access.AccessKeySecret))
-    if err != nil {
-        fmt.Println(err.Error())
-        return
-    }
+func run(config *myConfig.Config, cli *alidns.Client) {
+	// 查询IP
+	PubIPv4, PubIPv6 := myConfig.PublilcIPs(config.IPv4, config.IPv6)
 
-    // 查询IP
-    PubIPv4, PubIPv6 := access.PublilcIPs()
-
-    // 更新所有用户配置
-    for _, customer := range access.Customer {
-        if PubIPv4 != nil {
-            err = updateDomains(cli , customer.IPv4RR, customer.Domain, *PubIPv4, myConfig.IPv4Type, access.TTL)
-            if err != nil {
-                fmt.Printf("update IPv4 failed : %s\r\n", err.Error())
-            }
-        }
-        if PubIPv6 != nil {
-            err = updateDomains(cli , customer.IPv6RR, customer.Domain, *PubIPv6, myConfig.IPv6Type, access.TTL)
-            if err != nil {
-                fmt.Printf("update IPv6 failed : %s\r\n", err.Error())
-            }
-        }
-    }
+	// 更新所有用户配置
+	for _, customer := range config.Customer {
+		if PubIPv4 != nil {
+			err := updateDomains(cli, config, nil, customer.IPv4RR, customer.Domain, *PubIPv4, myConfig.IPv4Type, config.TTL)
+			if err != nil {
+				fmt.Printf("update IPv4 failed : %s\r\n", err.Error())
+			}
+		}
+		if PubIPv6 != nil {
+			err := updateDomains(cli, config, nil, customer.IPv6RR, customer.Domain, *PubIPv6, myConfig.IPv6Type, config.TTL)
+			if err != nil {
+				fmt.Printf("update IPv6 failed : %s\r\n", err.Error())
+			}
+		}
+	}
 }
 
-func updateDomains(cli *alidns.Client,DomainRR, Domain, IP string, ipType string, ttl int64) error {
-    // 查询域名解析记录
-    subDomain := DomainRR + "." + Domain
-    subDomainRequest := &alidns.DescribeSubDomainRecordsRequest{
-        SubDomain: &subDomain,
-    }
-    subDomainRecords, err := cli.DescribeSubDomainRecords(subDomainRequest)
-    if err != nil {
-        return err
-    }
+func runMultiBroadband(config *myConfig.Config, cli *alidns.Client) {
+	if config.BroadbandRetry < 2 {
+		fmt.Println("broadband_retry must be bigger than 1.")
+		return
+	}
+	// 重复获取公网IP
+	broadbandIPv4, broadbandIPv6 := myConfig.MultiBroadbandPublicIPs(config.IPv4, config.IPv6, config.BroadbandRetry)
 
-    // 获取域名id，找到后更新，找不到添加
-    for _, record := range subDomainRecords.Body.DomainRecords.Record {
-        // 类型一致才可以修改
-        if (*record.Type) == ipType {
-            // IP 没有发生变化，不做任何操作
-            if (*record.Value) == IP {
-                return nil
-            }
-            fmt.Printf("[%s] - New public IP : %s\r\n", time.Now().Format("2006-01-02 15:04:05"), IP)
-            // 修改域名解析记录
-            updateDomainRecordRequest := &alidns.UpdateDomainRecordRequest{
-                RecordId: record.RecordId,
-                RR:       record.RR,
-                Type:     record.Type,
-                Value:    &IP,
-            }
-            _, err = cli.UpdateDomainRecord(updateDomainRecordRequest)
-            if err != nil {
-                return err
-            }
-            return nil
-        }
-    }
-    // 没有找到Type一致的记录
-    AddDomainRecordRequest := &alidns.AddDomainRecordRequest{
-        DomainName:   &Domain,
-        RR:           &DomainRR,
-        Type:         &ipType,
-        Value:        &IP,
-        TTL:          &ttl,
-    }
-    _, err = cli.AddDomainRecord(AddDomainRecordRequest)
-    if err != nil {
-        return err
-    }
-    return nil
+	// 更新所有用户配置
+	for _, customer := range config.Customer {
+		if broadbandIPv4 != nil {
+			IP := myConfig.BroadbandIPFisrt(broadbandIPv4)
+			err := updateDomains(cli, config, broadbandIPv4, customer.IPv4RR, customer.Domain, IP, myConfig.IPv4Type, config.TTL)
+			if err != nil {
+				fmt.Printf("update IPv4 failed : %s\r\n", err.Error())
+			}
+		}
+		if broadbandIPv6 != nil {
+			IP := myConfig.BroadbandIPFisrt(broadbandIPv6)
+			err := updateDomains(cli, config, broadbandIPv6, customer.IPv6RR, customer.Domain, IP, myConfig.IPv6Type, config.TTL)
+			if err != nil {
+				fmt.Printf("update IPv6 failed : %s\r\n", err.Error())
+			}
+		}
+	}
 }
 
+
+func updateDomains(cli *alidns.Client, config *myConfig.Config, broadbandIP map[string]bool, DomainRR, Domain, IP, ipType string, ttl int64) error {
+	subDomain := DomainRR + "." + Domain
+	// 使用缓存
+	if config.UseCache {
+		cacheKey := myConfig.CacheKey(&subDomain, &ipType)
+		if myConfig.DoesIPChanged(config, broadbandIP, cacheKey, "", IP){
+			return nil
+		} else if config.IPsCache[cacheKey] == "" {
+			// 没有记录
+			AddDomainRecordRequest := &alidns.AddDomainRecordRequest{
+				DomainName: &Domain,
+				RR:         &DomainRR,
+				Type:       &ipType,
+				Value:      &IP,
+				TTL:        &ttl,
+			}
+			resp, err := cli.AddDomainRecord(AddDomainRecordRequest)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("[%s] - New public IP : %s => %s\r\n", time.Now().Format("2006-01-02 15:04:05"), cacheKey, IP)
+			// 更新缓存
+			config.IPsCache[cacheKey] = IP
+			config.RecordIds[cacheKey] = *resp.Body.RecordId
+			return nil
+		}
+		// 有记录，更新
+		recordId := config.RecordIds[cacheKey]
+		updateDomainRecordRequest := &alidns.UpdateDomainRecordRequest{
+			RecordId: &recordId,
+			RR:       &DomainRR,
+			Type:     &ipType,
+			Value:    &IP,
+		}
+		_, err := cli.UpdateDomainRecord(updateDomainRecordRequest)
+		if err != nil {
+			return err
+		}
+		config.IPsCache[cacheKey] = IP // 更新缓存
+		fmt.Printf("[%s] - New public IP : %s => %s\r\n", time.Now().Format("2006-01-02 15:04:05"), cacheKey, IP)
+		return nil
+	} else {
+		// 查询域名解析记录
+		subDomainRequest := &alidns.DescribeSubDomainRecordsRequest{
+			SubDomain: &subDomain,
+		}
+		subDomainRecords, err := cli.DescribeSubDomainRecords(subDomainRequest)
+		if err != nil {
+			return err
+		}
+
+		// 获取域名id，找到后更新，找不到添加
+		for _, record := range subDomainRecords.Body.DomainRecords.Record {
+			// 类型一致才可以修改
+			if (*record.Type) == ipType {
+				if myConfig.DoesIPChanged(config, broadbandIP, "", *record.Value, IP) {
+					return nil
+				}
+				fmt.Printf("[%s] - New public IP : %s\r\n", time.Now().Format("2006-01-02 15:04:05"), IP)
+				// 修改域名解析记录
+				updateDomainRecordRequest := &alidns.UpdateDomainRecordRequest{
+					RecordId: record.RecordId,
+					RR:       record.RR,
+					Type:     record.Type,
+					Value:    &IP,
+				}
+				_, err = cli.UpdateDomainRecord(updateDomainRecordRequest)
+				if err != nil {
+					return err
+				}
+				return nil
+			}
+		}
+		// 没有找到Type一致的记录
+		AddDomainRecordRequest := &alidns.AddDomainRecordRequest{
+			DomainName: &Domain,
+			RR:         &DomainRR,
+			Type:       &ipType,
+			Value:      &IP,
+			TTL:        &ttl,
+		}
+		_, err = cli.AddDomainRecord(AddDomainRecordRequest)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("[%s] - New public IP : %s\r\n", time.Now().Format("2006-01-02 15:04:05"), IP)
+		return nil
+	}
+}
 
 func main() {
-    flag.Parse()
-    if h {
-        flag.Usage()
-        return
-    }
-    if c == "" {
-        flag.Usage()
-        return
-    }
-    // 读取配置
-    var config myConfig.Config
-    err := config.InitConfig(c)
-    if err != nil {
-        fmt.Printf("Parse config file error : %s\r\n", err.Error())
-        return
-    }
+	flag.Parse()
+	if h {
+		flag.Usage()
+		return
+	}
+	if c == "" {
+		flag.Usage()
+		return
+	}
+	// 读取配置
+	var config myConfig.Config
+	err := config.InitConfig(c)
+	if err != nil {
+		fmt.Printf("Parse config file error : %s\r\n", err.Error())
+		return
+	}
 
-    for {
-        run(config)
-        time.Sleep(time.Second * time.Duration(config.Interval))
-    }
+	// 创建客户端
+	cli, err := CreateClient(tea.String(config.AccessKeyId), tea.String(config.AccessKeySecret))
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// 加载缓存
+	config.InitCache(cli)
+
+	interval := time.Second * time.Duration(config.Interval)
+	// 根据宽带多拨情况来判断使用哪个函数主体运行
+	var runFunc func(config2 *myConfig.Config, client *alidns.Client)
+	if config.BroadbandRetry < 2 {
+		fmt.Println("正常DDNS")
+		runFunc = run
+	} else {
+		fmt.Println("宽带多拨DDNS")
+		runFunc = runMultiBroadband
+	}
+
+	for {
+		runFunc(&config, cli)
+		time.Sleep(interval)
+	}
 }
